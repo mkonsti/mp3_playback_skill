a5856ec8e680b84baaceff13fcefdea0be5dcfcb
Marcel <marcel.konstantinov@gmail.com>
2018-02-02 19:22:54 +0100

    change in init

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
-# The MIT License (MIT)	
-# 
+# The MIT License (MIT)
+#
 # Copyright (c) 2016 Ethan Ward
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -74,10 +74,6 @@ class MP3PlaybackSkill(MycroftSkill):
         intent = IntentBuilder('PauseIntent').require('PauseKeyword')
         self.register_intent(intent, self.handle_pause)
 
-        intent = IntentBuilder('PlayIntent') \
-            .require('ResumeKeyword')
-        self.register_intent(intent, self.handle_play)
-
     def handle_play_song_intent(self, message):
         # Play the song requested
         title = message.metadata.get("SongTitle")
@@ -96,9 +92,6 @@ class MP3PlaybackSkill(MycroftSkill):
     def handle_pause(self, message):
         self.audio_service.pause()
 
-    def handle_play(self, message):
-        """Resume playback if paused"""
-        self.audio_service.resume()
 
     def handle_play_music_intent(self, message):
         # Play any song
